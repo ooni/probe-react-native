2c9c7227d58c9013e1d78be49e5bc87eeba677b7
Sarath M S <sarath.ms@gmail.com>
2019-10-16 23:19:11 -0400

    Fix lint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+import React from 'react'
+import {AppRegistry, StyleSheet, Text, View} from 'react-native'
 
 import About from './views/About'
 
@@ -9,7 +9,7 @@ class HelloWorld extends React.Component {
       <View style={styles.container}>
         <Text style={styles.hello}>Hello, World</Text>
       </View>
-    );
+    )
   }
 }
 var styles = StyleSheet.create({
@@ -22,7 +22,7 @@ var styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-});
+})
 
-AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
+AppRegistry.registerComponent('HelloWorld', () => HelloWorld)
 AppRegistry.registerComponent('About', () => About)
